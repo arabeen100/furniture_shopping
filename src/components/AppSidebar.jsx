9ef1508b31cbfa3372cb167760bb33f4992c9a32
arabeen100 <mahmoudarabeen@gmail.com>
2025-03-06 17:55:51 +0200

    last last commit

--- src/components/AppSidebar.jsx
+++ src/components/AppSidebar.jsx
@@ -23,7 +23,10 @@ export function AppSidebar() {
                 <li><Link to='/categories/ستائر'>ستائر</Link></li>
                 <li><Link to='/categories/any'>any</Link></li>
                 <li><Link to='profile/Personalinfo' className={`${!registered&& " hidden"} md:hidden relative top-2 left-64 w-[38px] h-[38px]`} ><img src={profileLogo} width='25' alt="profile-logo"loading="lazy"/></Link></li>
-                <li ><Link onClick={()=>{dispatch(ifNotRegister())}} to='/auth/login' className={`${!registered&& " hidden"} w-[60px] h-[40px] bg-white outline-0  \cursor-pointer                border  text-sm border-black grid place-content-center relative top-6 left-56 `}>logout</Link ></li>
+                <li ><Link onClick={()=>{dispatch(ifNotRegister());
+                dispatch(setExpanded());
+
+                }} to='/auth/login' className={`${!registered&& " hidden"} w-[60px] h-[40px] bg-white outline-0  \cursor-pointer                border  text-sm border-black grid place-content-center relative top-6 left-56 `}>logout</Link ></li>
               </ul>
               <ul className={`${!registered?" inline-flex gap-3":"hidden"} relative left-13 top-9`}>
                 <li><Link onClick={()=>{dispatch(setExpanded())}} to="/auth/login" className={`w-[140px] h-[30px]outline-none p-1 rounded-[5px] bg-white border border-black grid  place-content-center `}>تسجيل الدخول</Link></li>
