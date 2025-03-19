2301142899eb36bcfc873fb071e1178256a57e95
arabeen100 <mahmoudarabeen@gmail.com>
2025-03-19 20:04:15 +0200

    some other changes

--- src/components/Plusbutton.jsx
+++ src/components/Plusbutton.jsx
@@ -9,15 +9,15 @@ const Plusbutton = () => {
   return (
     <div className=' plusButton flex gap-7  w-fit'>
         <div className='w-14 grid place-content-center  '>
-           <div  onClick={()=>{dispatch(setButtonClicked())}} className={` fixed bottom-10 ${buttonClicked&&"bottom-23"} left-4 cursor-pointer size-13 hover:scale-115  transition-all duration-300 rounded-full bg-[#5bb3ae] grid place-content-center`}><Plusmarkicon/></div>
+           <div  onClick={()=>{dispatch(setButtonClicked())}} className={` fixed z-50 bottom-10 ${buttonClicked&&"bottom-23"} left-4 cursor-pointer size-13 hover:scale-115  transition-all duration-300 rounded-full bg-[#5bb3ae] grid place-content-center`}><Plusmarkicon/></div>
         </div>
         <div className={` ${!buttonClicked&&"hidden"}  w-fit transition-all duration-600 `}>
             <Link to="/contactUs" onClick={()=>{dispatch(setButtonClicked())}}  className=' cursor-pointer w-fit'>
-                <div   className=' cursor-pointer fixed bottom-40 grid place-content-center size-13 transition-all duration-300 hover:scale-115  border-1 border-[#5bb3ae] rounded-full'><Phoneicon/></div>
+                <div   className=' cursor-pointer fixed  z-50 bottom-40 grid place-content-center size-13 transition-all duration-300 hover:scale-115  border-1 border-[#5bb3ae] rounded-full'><Phoneicon/></div>
                 <p className='fixed bottom-32 left-20 text-[#042e2e]'>اتصل بنا</p>
             </Link>
             <Link  to='/FAQ' onClick={()=>{dispatch(setButtonClicked())}} className=' cursor-pointer w-fit '>
-                <div className=' fixed bottom-10 grid place-content-center size-13 hover:scale-115 transition-all duration-300 border-1 border-[#5bb3ae] rounded-full text-lg text-[#5bb3ae]  font-extrabold'>?</div>
+                <div className=' fixed z-50 bottom-10 grid place-content-center size-13 hover:scale-115 transition-all duration-300 border-1 border-[#5bb3ae] rounded-full text-lg text-[#5bb3ae]  font-extrabold'>?</div>
                 <p className='fixed bottom-3 left-23.5 text-[#042e2e]'>FAQ</p>
             </Link>
         </div>
