ecdd4edfdbb108b1fe57079b9452b9440e36623c
arabeen100 <mahmoudarabeen@gmail.com>
2025-07-17 17:43:21 +0200

    simple change

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice= createApi({
     reducerPath:'api',
     baseQuery: fetchBaseQuery({
-        baseUrl:"https://al-rahman-moebel.eu/api",
+        baseUrl:"https://alrhman.wisteirunuon-cheecktraacking.online/api",
     }),
     endpoints:(builder)=>({
         register:builder.mutation({
